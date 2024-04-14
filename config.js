5a5f5c2879349c78a0ccea32b87db15866640dbb
maryam22001 <129103068+maryam22001@users.noreply.github.com>
2024-04-14 18:03:03 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PORT =8080;
+export const mongoDBURL=
+
+'mongodb+srv://maryamosman0099:<password>@book-store-mern.mjyysjv.mongodb.net/'
+
+const MONGODB_URI = 'mongodb+srv://maryamosman0099:<password>@book-store-mern.mjyysjv.mongodb.net/<dbname>?retryWrites=true&w=majority';
